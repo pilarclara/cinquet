e1dab81c1aea2469612854c291d3df2d435316ea
DESKTOP-0ILKU60\pilar <pilarclara@gmail.com>
2021-10-23 19:26:44 +0200

    creada las manos de los jugadores, con false si no tiene la baraja y true si la tiene

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ class Carta{
     nombre_Carta(){ return `${this._palo}_${this._valor}`}
 }
 
+class Mano_Cartas{
+    constructor(cartasElegidas){
+        this._cartas= [];
+        for (let i=0; i<4; i++){
+            this._cartas[i]=[];
+            for (let k=0; k<10; k++){
+                cartasElegidas.includes(i*4+k)?this._cartas[i][k]= true: this._cartas[i][k]= false;
+            } 
+        }
+        //cartasElegidas.forEach(el => {this._cartas[el/10][el%10] = true }) esto no funciona, inicialmente todo a false y aquí a true pero me daba error, no sé porqué
+    }
+    get cartas(){ return this._cartas}
+}
+
 const nombre_palo = ['oro', 'copa', 'espada', 'basto'];
 const valor_carta = ['1','2','3','4','5','6','7','sota', 'caballo','rey'];
 
@@ -25,10 +39,10 @@ nombre_palo.forEach((palo, indexPalo) => {
 });
 console.log(baraja);
 
-let jugadores = [];
+let distribucion_cartas = [];
 let cant_jugadores = 3;
 
-for(let i= 0; i< cant_jugadores; i++){ jugadores[i]= []}
+for(let i= 0; i< cant_jugadores; i++){ distribucion_cartas[i]= []}
 
 cartasARepartir = [];
 for (let i = 0; i<40; i++){ cartasARepartir[i]=i}
@@ -36,6 +50,12 @@ for (let i = 0; i<40; i++){ cartasARepartir[i]=i}
 jugadorInicial = numAleatorio(1, cant_jugadores);
 
 for (let i=0; i<40; i++){
-    jugadores[(i+jugadorInicial)%cant_jugadores].push(cartasARepartir.splice(numAleatorio(0,39-i), 1)[0]);
+    distribucion_cartas[(i+jugadorInicial)%cant_jugadores].push(cartasARepartir.splice(numAleatorio(0,39-i), 1)[0]);
+}
+console.log(distribucion_cartas);
+
+let jugadores = [];
+for (let i=0; i<cant_jugadores; i++){
+    jugadores[i]= new Mano_Cartas(distribucion_cartas[i]);
 }
 console.log(jugadores);

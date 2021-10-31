b00c758bd49d0414c5071c05f69ecd5bcd055918
DESKTOP-0ILKU60\pilar <pilarclara@gmail.com>
2021-10-31 23:22:18 +0100

    la primera jugada si es ordenador

--- cinquet.js
+++ cinquet.js
@@ -8,18 +8,28 @@ class Mano_Cartas{
     }
     get cartas(){ return this._cartas}
     numero_cartas(){ return this._cartas.length}
+    eliminar_carta(carta){ this._cartas.splice(this._cartas.indexOf(carta),1); }
 }
+
+class Mano_Cartas_Ordenador extends Mano_Cartas{
+}
+
+class Mano_Cartas_Jugador extends Mano_Cartas{
+}
+
 class Partida{
     constructor(cant_jugadores,dificultad){
         this._cantidad_jugadores = cant_jugadores;
-        this._jugadores = [];
-        for (let i=0; i<cant_jugadores; i++){
-            this._jugadores[i]= new Mano_Cartas(this.repartirCartas()[i]);
+        let cartas_Repartidas = this.repartirCartas();
+        this._jugadores=[];
+        this._jugadores.push(new Mano_Cartas_Jugador(cartas_Repartidas[0]));
+        for (let i=1; i<cant_jugadores; i++){
+            this._jugadores.push(new Mano_Cartas_Ordenador(cartas_Repartidas[i]));
         }
-        console.log(this._jugadores);
+        this._posiblesJugadas = [4];
         this._turno = this.buscar5oro();
-
-        // _turno +=1%cantidad_jugadores
+        if (this._turno) this.jugado(4);
+        console.log(this._posiblesJugadas);
     }
     repartirCartas(){
         let cartas_Repartidas = [];
@@ -31,7 +41,6 @@ class Partida{
             cartas_Repartidas[(i+primeroARepartir)%this._cantidad_jugadores].push(cartasARepartir.splice(numAleatorio(0,39-i), 1)[0]);
         }
         cartas_Repartidas.forEach(e=>e.sort((a,b)=>a-b));
-        console.log(cartas_Repartidas);
         return cartas_Repartidas;
     }    
     buscar5oro(){
@@ -40,10 +49,16 @@ class Partida{
         this._jugadores.forEach((element, index) => {if (element.cartas.includes(4)) inicial = index});
         return inicial
     }
-
+    jugado(carta){
+        this._jugadores[this._turno].eliminar_carta(carta);
+        this._posiblesJugadas.splice(this._posiblesJugadas.indexOf(carta));
+        if (carta%10>0 && carta%10<5) this._posiblesJugadas.push(carta-1);
+        if (carta%10<9 && carta%10>3) this._posiblesJugadas.push(carta+1);
+    }
     devolverManoJugador(num_jugador){
         return this._jugadores[num_jugador];
     }
+    //cartasJugadas
 }
 
 export {Partida, Mano_Cartas}
